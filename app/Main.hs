044c623499e96a6321c94e9fcfa8a04577ca1475
Matthew Gordon <gordon@qps.nl>
2017-06-24 23:42:04 -0300

    Convert image to a repa float array and back.

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,11 @@ module Main where
 import Lib
 
 import Codec.Picture
+import Data.Array.Repa
+import Data.Array.Repa.Repr.Vector
+import Data.Convertible
 import Data.Semigroup ((<>))
+import qualified Data.Vector.Storable
 import Options.Applicative
 import qualified System.Environment
 import System.Exit
@@ -40,7 +44,47 @@ main = main' =<< execParser opts
 main' :: CommandLineArgs -> IO ()
 main' (CommandLineArgs inputFileName outputFileName) = readImage inputFileName >>= \case
     Right image -> do
-      savePngImage outputFileName image
+      print (typeName image)
+      savePngImage outputFileName $ ( rgbFloatArrayToImage . doProcessing . imageToRgbFloatArray . convertRGB8) image
     Left message -> do
          print message
          exitWith (ExitFailure (-2))
+
+typeName :: DynamicImage -> String
+typeName (ImageY8 _) = "ImageY8"
+typeName (ImageY16 _) = "ImageY16"
+typeName (ImageYF _) = "ImageYF"
+typeName (ImageYA8 _) = "ImageYA8"
+typeName (ImageYA16 _) = "ImageYA16"
+typeName (ImageRGB8 _) = "ImageRGB8"
+typeName (ImageRGB16 _) = "ImageRGB16"
+typeName (ImageRGBF _) = "ImageRGBF"
+typeName (ImageRGBA8 _) = "ImageRGBA8"
+typeName (ImageRGBA16 _) = "ImageRGBA16"
+typeName (ImageYCbCr8 _) = "ImageYCbCr8"
+typeName (ImageCMYK8 _) = "ImageCMYK8"
+typeName (ImageCMYK16 _) = "ImageCMYK16"
+
+
+-- Convert image to a repa array of floats.
+-- First converts the image to [Float], which I can't image is the best way to go about it, but I can't figure
+-- out how to do it better.
+imageToRgbFloatArray image =
+    ((rgb8ArrayForImage (imageWidth image) (imageHeight image)) . uint8VectorToFloatList) image
+
+
+rgb8ArrayForImage :: Int -> Int -> [Float] -> Array U DIM3 Float
+rgb8ArrayForImage width height = fromListUnboxed (Z :. (width::Int) :. (height::Int) :. (3::Int))
+
+uint8VectorToFloatList = (Prelude.map ((/256.0).fromIntegral)) . Data.Vector.Storable.toList . imageData
+
+rgbFloatArrayToImage :: Array U DIM3 Float -> DynamicImage
+rgbFloatArrayToImage array =
+    (ImageRGB8 . (Image width height) . Data.Vector.Storable.fromList . (Prelude.map (convert.(*256.0))) . toList) array
+        where width = ((!!2) . listOfShape . extent ) array
+              height = ((!!1) . listOfShape . extent ) array
+
+
+
+doProcessing :: Array U DIM3 Float -> Array U DIM3 Float
+doProcessing = id
